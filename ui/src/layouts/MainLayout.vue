9d4a61375eb8e45448bc570a3d95142e3b5a7805
Petr <lenor.ag@gmail.com>
2020-11-23 00:47:39 +0100

    added sign in and out responsible buttons, menu show up on smal windows size

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -2,13 +2,25 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat dense round icon="home" aria-label="home" to="/" />
-
-        <q-toolbar-title>Msf-pvp.com</q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-
-        <q-btn color="primary" label="Registration" to="registration" />
+        <q-toolbar-title>
+          msf-pvp
+          <!-- <q-img src="static/marvelstrikeforce_lob_log_01.png"></q-img>-->
+        </q-toolbar-title>
+        <div class="gt-sm">
+          <q-btn outline rounded label="sign up" class="q-mr-sm"></q-btn>
+          <q-btn outline rounded label="sign in"></q-btn>
+        </div>
+        <div>
+          <q-btn
+            class="lt-md"
+            flat
+            dense
+            round
+            @click="leftDrawerOpen = !leftDrawerOpen"
+            icon="menu"
+            aria-label="Menu"
+          />
+        </div>
       </q-toolbar>
     </q-header>
 
